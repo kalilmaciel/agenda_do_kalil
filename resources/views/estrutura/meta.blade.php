837a17024056049f4628ba08a51996315e2bd9d9
Kalil Maciel <kalilmaciel@gmail.com>
2025-03-18 16:18:13 -0300

    Ajustando a forma de compartilhar a aplicação no Whatsapp/Facebook

---
+++ resources/views/estrutura/meta.blade.php
@@ -0,0 +1,15 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>{{ env('APP_NAME') }}</title>
+<meta name="robots" content="noindex" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<meta name="description" content="Sistema de gestão de amigos e contatos." />
+<meta name="keywords" content="sistema amigos contatos telefone localizacao endereco" />
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="mobile-web-app-capable" content="yes" />
+<meta property="og:title" content="{{ env('APP_NAME') }}" />
+<meta property="og:url" content="{{ env('APP_URL') }}" />
+<meta property="og:og:site_name" content="{{ env('APP_NAME') }}" />
+<meta property="og:type" content="website" />
+<meta property="og:image" content="{{ asset('assets/img/favicon.png') }}" />
--- resources/views/estrutura_externa.blade.php
+++ resources/views/estrutura_externa.blade.php
@@ -2,22 +2,8 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ env('APP_NAME') }}</title>
 
-    <meta name="robots" content="noindex" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-    <meta name="description" content="Sistema Gestão de contatos." />
-    <meta name="keywords" content="sistema contatos pessoas telefone localizacao" />
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="mobile-web-app-capable" content="yes" />
-    <meta property="og:title" content="{{env('APP_SITE')}}" />
-    <meta property="og:url" content="{{env('APP_URL')}}" />
-    <meta property="og:og:site_name" content="{{env('APP_SITE')}}" />
-    <meta property="og:type" content="website" />
-    <meta property="og:image" content="{{asset('assets/img/favicon.png')}}" />
+    @include('estrutura.meta')
 
     @include('estrutura.favicon')
 
